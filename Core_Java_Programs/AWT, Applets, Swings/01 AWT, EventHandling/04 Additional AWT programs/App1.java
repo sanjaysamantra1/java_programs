d5a0e2d215088eeffcee5ac45c9c0de2bf5aea6b
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-12-06 23:25:20 +0530

    AWT removed

--- Core_Java_Programs/AWT, Applets, Swings/01 AWT, EventHandling/04 Additional AWT programs/App1.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import java.applet.*;
-public class App1 extends Applet{
-      public void paint(Graphics g){
-         g.setFont(new Font("Impact",Font.PLAIN,32));
-         g.setColor(Color.red);
-         g.drawString("My First Applet",50,150);
-      }//paint() closed
-}//App closed
--- Core_Java_Programs/AWT, Applets, Swings/03 Swings/ScrollableJPopupmenu/XConstant.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-
-public interface XConstant {
-	public static final Icon EMPTY_IMAGE_ICON = new ImageIcon("menu_spacer.gif");
-}
